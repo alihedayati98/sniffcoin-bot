8ca2cb29c1e298af0c4e22d7026d3fbb552152a9
alihedayati98 <ali.hedayati77@yahoo.com>
2025-04-19 01:41:26 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-import telebot
-import os
-
-API_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
-
-bot = telebot.TeleBot(API_TOKEN)
-
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "سلام شکارچی! من آماده‌ام پامپ بعدی رو واست بو بکشم!")
-
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, "فعلاً در حال یادگیری شکارم...")
-
-bot.polling()
+from sniffbot import bot
